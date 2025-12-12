fc2ab3bc396bd5d77b8413e44fdd4c910cd84a9b
Brain-C-IBPS <lucile.megret@upmc.fr>
2025-12-12 12:50:34 +0100

    Update 9_calculate_BSreplicates_edge_presence.py

--- SCRIPTS/9_calculate_BSreplicates_edge_presence.py
+++ SCRIPTS/9_calculate_BSreplicates_edge_presence.py
@@ -15,6 +15,7 @@ ENSEMBL IDs to gene symbols
 ------------------------------------------------------------------------------
 
 Contributors (alphabetic order): Maialen Arrieta-Lobo (1) 
+                                 Lucile Mégret (1)
                                  Christian Neri (1)
     
 Laboratories:   
@@ -27,7 +28,7 @@ Affiliations:
 (1) Institut National de la Santé et de la Recherche Médicale (INSERM)
 
 ==============================================================================
-|      Copyright (C) 2023 Maialen Arrieta-Lobo and Christian Neri            | 
+| Copyright (C) 2023 Maialen Arrieta-Lobo, Lucile Mégret and Christian Neri  | 
 ==============================================================================
 |                                                                            |
 |  This work is licensed under the Creative Commons                          | 
