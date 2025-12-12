9577ba88b5e5d72f85aa9d4af4bf3065f40fdccc
Brain-C-IBPS <lucile.megret@upmc.fr>
2025-12-12 12:39:22 +0100

    Update 10_filter_network_BSreplicates_reference_percentage.py

--- SCRIPTS/10_filter_network_BSreplicates_reference_percentage.py
+++ SCRIPTS/10_filter_network_BSreplicates_reference_percentage.py
@@ -20,6 +20,7 @@ ENSEMBL IDs to gene symbols.
 ------------------------------------------------------------------------------
 
 Contributors (alphabetic order): Maialen Arrieta-Lobo (1) 
+                                 Lucile Mégret
                                  Christian Neri (1)
     
 Laboratories:   
@@ -32,7 +33,7 @@ Affiliations:
 (1) Institut National de la Santé et de la Recherche Médicale (INSERM)
 
 ==============================================================================
-|      Copyright (C) 2023 Maialen Arrieta-Lobo and Christian Neri            | 
+|  Copyright (C) 2023 Maialen Arrieta-Lobo, Lucile Mégret and Christian Neri            | 
 ==============================================================================
 |                                                                            |
 |  This work is licensed under the Creative Commons                          | 
